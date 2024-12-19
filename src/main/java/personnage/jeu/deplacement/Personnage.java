97d383bfa74384324cdb35f4bdea68a5d39b6f4d
Mouad-Ag <mouadag10@gmail.com>
2024-12-19 15:40:07 +0100

    refactor cycle 2

--- src/main/java/personnage/jeu/deplacement/Personnage.java
+++ src/main/java/personnage/jeu/deplacement/Personnage.java
@@ -3,7 +3,7 @@ package personnage.jeu.deplacement;
 public class Personnage {
     public String tourner(int i) {
         if (i % 4 == 1) return "EST";
-        if (i == 2) return "SUD";
+        if (i % 4 == 2) return "SUD";
         return null;
     }
 }
